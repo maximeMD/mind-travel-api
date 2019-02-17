ec7a0a53f9b825ac879fa3be4b3d96ed15296fb3
MaximeMD <maxime.martydessus@gmail.com>
2019-02-17 19:02:12 +0100

    add GET image/thumb

--- src/api/albums.js
+++ src/api/albums.js
@@ -17,7 +17,7 @@ albumsRouter.get('/', (req, res) => {
   });
 
   var s3Params = {
-    Bucket: awsConfig.awsS3BucketName
+    Bucket: awsConfig.awsS3BucketNameImages
   };
   var s3 = new aws.S3();
 
@@ -52,99 +52,3 @@ albumsRouter.get('/', (req, res) => {
   });
 });
 export default albumsRouter;
-
-// export default ({ config, db }) =>
-//   resource({
-//     /** Property name to store preloaded entity on `request`. */
-//     id: 'album',
-
-//     /** For requests with an `id`, you can auto-load the entity.
-//      *  Errors terminate the request, success sets `req[id] = data`.
-//      */
-//     // load(req, id, callback) {
-//     //   let facet = facets.find(facet => facet.id === id),
-//     //     err = facet ? null : 'Not found';
-//     //   callback(err, facet);
-//     // },
-
-//     /** GET / - List all entities */
-//     index({ params }, res) {
-//       // var albums = [{ key: 'test', pictures: ['test', 'test'] }];
-//       var albums = [];
-
-//       var newAlbumKey = '';
-//       var albumImages = [];
-//       var imageKey = '';
-
-//       aws.config.update({
-//         secretAccessKey: awsConfig.awsSecretKey,
-//         accessKeyId: awsConfig.awsAccessKey,
-//         region: awsConfig.awsS3Region
-//       });
-
-//       var s3Params = {
-//         Bucket: awsConfig.awsS3BucketName
-//       };
-//       var s3 = new aws.S3();
-
-//       s3.listObjectsV2(s3Params, function(err, data) {
-//         if (err) console.log(err, err.stack);
-//         else {
-//           data.Contents.forEach(item => {
-//             var albumKey = item.Key.split('/')[0];
-//             var pictureKey = item.Key.split('/')[1]; // null if the item is an album
-
-//             if (pictureKey) {
-//               // this item is a picture
-//               albums.map(album => {
-//                 if (album.key == albumKey) {
-//                   album.pictures.push(item.Key);
-//                   if (pictureKey == '_thumb') {
-//                     album.thumbnail = item.Key;
-//                   }
-//                 }
-//               });
-//             } else {
-//               // this item is an album
-//               newAlbumKey = item.Key.split('/')[0];
-//               albums.push({
-//                 key: newAlbumKey,
-//                 pictures: []
-//               });
-//             }
-//           });
-//         }
-//         res.json(albums);
-//       });
-
-//       // res.json(albums);
-//     }
-
-//     /** POST / - Create a new entity */
-//     // create({ body }, res) {
-//     //   body.id = facets.length.toString(36);
-//     //   facets.push(body);
-//     //   res.json(body);
-//     // },
-
-//     /** GET /:id - Return a given entity */
-//     // read({ facet }, res) {
-//     //   res.json(facet);
-//     // },
-
-//     /** PUT /:id - Update a given entity */
-//     // update({ facet, body }, res) {
-//     //   for (let key in body) {
-//     //     if (key !== 'id') {
-//     //       facet[key] = body[key];
-//     //     }
-//     //   }
-//     //   res.sendStatus(204);
-//     // },
-
-//     /** DELETE /:id - Delete a given entity */
-//     // delete({ facet }, res) {
-//     //   facets.splice(facets.indexOf(facet), 1);
-//     //   res.sendStatus(204);
-//     // }
-//   });
--- src/api/pictures.js
+++ src/api/pictures.js
@@ -5,20 +5,37 @@ import { Router } from 'express';
 
 var imagesRouter = Router();
 
+aws.config.update({
+  secretAccessKey: awsConfig.awsSecretKey,
+  accessKeyId: awsConfig.awsAccessKey,
+  region: awsConfig.awsS3Region
+});
+const s3 = new aws.S3();
+
 /* Get an image */
 imagesRouter.get('/:id', (req, res) => {
-  aws.config.update({
-    secretAccessKey: awsConfig.awsSecretKey,
-    accessKeyId: awsConfig.awsAccessKey,
-    region: awsConfig.awsS3Region
-  });
-  var s3Params = {
-    Bucket: awsConfig.awsS3BucketName,
+  console.log('Retrieve image with key : ' + req.params.id);
+  const s3ParamsGetImage = {
+    Bucket: awsConfig.awsS3BucketNameImages,
     Key: req.params.id
   };
-  var s3 = new aws.S3();
-
-  s3.getObject(s3Params, function(err, data) {
+  s3.getObject(s3ParamsGetImage, function(err, data) {
+    if (err) console.log(err);
+    else {
+      res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+      res.write(data.Body, 'binary');
+      res.end(null, 'binary');
+    }
+  });
+});
+/* Get an image thumbnail */
+imagesRouter.get('/thumb/:id', (req, res) => {
+  console.log('Retrieve image thumbnail with key : ' + req.params.id);
+  const s3ParamsGetImageThumb = {
+    Bucket: awsConfig.awsS3BucketNameImagesThumbnails,
+    Key: req.params.id
+  };
+  s3.getObject(s3ParamsGetImageThumb, function(err, data) {
     if (err) console.log(err);
     else {
       res.writeHead(200, { 'Content-Type': 'image/jpeg' });
