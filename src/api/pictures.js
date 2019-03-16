c4a59909174eedc7b98cff3cb0c0f8b1261468d3
MaximeMD <maxime.martydessus@gmail.com>
2019-03-16 16:11:19 +0100

    [FEAT] set new jwt logic for picture thumb

--- src/api/pictures.js
+++ src/api/pictures.js
@@ -9,19 +9,19 @@ var imagesRouter = Router();
 aws.config.update({
   secretAccessKey: credentials.awsSecretKey,
   accessKeyId: credentials.awsAccessKey,
-  region: credentials.awsS3Region,
+  region: credentials.awsS3Region
 });
 const s3 = new aws.S3();
 
 /* Get an image */
 imagesRouter.get('/:id/:token', (req, res) => {
-  console.log('Verivy user token: ' + req.params.token);
-  console.log('credentials.jwtSecret:', credentials.jwtSecret);
+  // console.log('Verivy user token: ' + req.params.token);
+  // console.log('credentials.jwtSecret:', credentials.jwtSecret);
   if (req.params.token && jwt.verify(req.params.token, credentials.jwtSecret)) {
     console.log('Retrieve image with key : ' + req.params.id);
     const s3ParamsGetImage = {
       Bucket: credentials.awsS3BucketNameImages,
-      Key: req.params.id,
+      Key: req.params.id
     };
     s3.getObject(s3ParamsGetImage, function(err, data) {
       if (err) console.log(err);
@@ -36,19 +36,23 @@ imagesRouter.get('/:id/:token', (req, res) => {
   }
 });
 /* Get an image thumbnail */
-imagesRouter.get('/thumb/:id', (req, res) => {
-  console.log('Retrieve image thumbnail with key : ' + req.params.id);
-  const s3ParamsGetImageThumb = {
-    Bucket: credentials.awsS3BucketNameImagesThumbnails,
-    Key: req.params.id,
-  };
-  s3.getObject(s3ParamsGetImageThumb, function(err, data) {
-    if (err) console.log(err);
-    else {
-      res.writeHead(200, { 'Content-Type': 'image/jpeg' });
-      res.write(data.Body, 'binary');
-      res.end(null, 'binary');
-    }
-  });
+imagesRouter.get('/thumb/:id/:token', (req, res) => {
+  if (req.params.token && jwt.verify(req.params.token, credentials.jwtSecret)) {
+    console.log('Retrieve image thumbnail with key : ' + req.params.id);
+    const s3ParamsGetImageThumb = {
+      Bucket: credentials.awsS3BucketNameImagesThumbnails,
+      Key: req.params.id
+    };
+    s3.getObject(s3ParamsGetImageThumb, function(err, data) {
+      if (err) console.log(err);
+      else {
+        res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+        res.write(data.Body, 'binary');
+        res.end(null, 'binary');
+      }
+    });
+  } else {
+    res.sendStatus(401);
+  }
 });
 export default imagesRouter;
