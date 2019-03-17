14e976a9d9554ba1399fc2a9e2c2fda8e89392c1
MaximeMD <maxime.martydessus@gmail.com>
2019-03-17 20:41:27 +0100

    [FEAT] crypto password

--- src/api/users.js
+++ src/api/users.js
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import aws from 'aws-sdk';
 import * as credentials from '../credentials.json';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import hash from 'hash.js';
 
 var usersRouter = Router();
 
@@ -16,8 +18,6 @@ const table = 'mind-travel-users';
 
 // hash pass
 const saltRounds = 10;
-const myPlaintextPassword = 'myPass';
-const someOtherPlaintextPassword = 'not_bacon';
 
 /* Authenticate user */
 usersRouter.post('/authenticate', (req, res) => {
@@ -37,21 +37,63 @@ usersRouter.post('/authenticate', (req, res) => {
         JSON.stringify(err, null, 2),
       );
       res.sendStatus(401);
-    } else {
-      const user = data.Item;
-      //check if the password is correct
-      if (req.body.password === user.password) {
-        const jwtSecret = credentials.jwtSecret;
-        const token = jwt.sign({ sub: user.username }, jwtSecret);
-        const { password, ...userWithoutPassword } = user;
-        res.json({
-          ...userWithoutPassword,
-          token,
-        });
-      } else {
-        res.sendStatus(401);
-      }
+      return;
     }
+    if (!data.Item) {
+      console.error('User does not exist');
+      res.sendStatus(401);
+      return;
+    }
+    const user = data.Item;
+    console.log(user);
+    //check if the password is correct
+
+    bcrypt
+      .compare(req.body.password, user.password)
+      .then(isPasswordCorrect => {
+        if (isPasswordCorrect) {
+          const jwtSecret = credentials.jwtSecret;
+          const token = jwt.sign({ sub: user.username }, jwtSecret);
+          const { password, ...userWithoutPassword } = user;
+          res.json({
+            ...userWithoutPassword,
+            token,
+          });
+        } else {
+          res.sendStatus(401);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.sendStatus(500);
+      });
   });
+});
+
+usersRouter.post('/', (req, res) => {
+  bcrypt
+    .hash(req.body.password, saltRounds)
+    .then(hash => {
+      const dynamoPutUserParams = {
+        TableName: table,
+        Item: {
+          username: req.body.username,
+          password: hash,
+        },
+      };
+      docClient.put(dynamoPutUserParams, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.sendStatus(500);
+        } else {
+          console.log('PutUser succeeded, username : ' + req.body.username);
+          res.sendStatus(201);
+        }
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      res.sendStatus(500);
+    });
 });
 export default usersRouter;
